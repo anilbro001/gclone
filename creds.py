0eea63a42fdc881bc68f7ea615fef0ae06fe857f
anilbro001 <anilbro001@gmail.com>
2021-03-26 10:52:13 +0530

    Add files via upload

---
+++ creds.py
@@ -0,0 +1,21 @@
+class Creds():
+    # ENTER Your bot Token Here
+    TG_TOKEN = "993249913:AAEhx-MuyeVic0C4Ug0HAMnLlQFwc06lqFE"
+    
+    
+    
+    #  Make Sure You Are Providing both value if you need Teamdrive upload
+    # Because of pydrive And pydrive v2 Api
+    
+    #Folder Id Of Teamdrive
+    TEAMDRIVE_FOLDER_ID = "1FJtX16yjIyX7AmoaH6m3jWKSUUy2b39N"
+    
+    # Id of Team drive 
+    TEAMDRIVE_ID = "1FJtX16yjIyX7AmoaH6m3jWKSUUy2b39N"
+    
+    
+    
+    #Example 
+    #TG_TOKEN = "dkjfksdkffdkfdkfdj"
+    #TEAMDRIVE_FOLDER_ID = "13v4MaZnBz-iEHlZ0gFXk7rh"
+    #TEAMDRIVE_ID = "0APh6R4WVvguEUk9PV"
